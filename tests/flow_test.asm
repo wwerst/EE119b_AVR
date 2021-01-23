e8634539a2e1b7615d90ea1d2080ff460209b030
Will Werst <werstiv@gmail.com>
2021-01-23 15:53:03 -0800

    Add all stub functions

---
+++ tests/flow_test.asm
@@ -0,0 +1,21 @@
+; Tests the flow control instructions
+
+
+; Notes:
+;
+
+
+;PREPROCESS TestLD
+;PREPROCESS TestLDD
+;PREPROCESS TestLDI
+;PREPROCESS TestLDS
+;PREPROCESS TestMOV
+;PREPROCESS TestST
+;PREPROCESS TestSTD
+;PREPROCESS TestSTS
+;PREPROCESS TestPOP
+;PREPROCESS TestPUSH
+
+    NOP;
+
+
